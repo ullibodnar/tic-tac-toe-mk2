a17654542c390c1ba1a7286f413f8d5d3f1000b8
Ulli Bodnar <ullibodnar@gmail.com>
2018-07-22 13:28:44 +1200

    Add first pass of game board

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export default function App () {
-  return <h1>Hello Bro</h1>
+  return (
+    <div>
+      <div>0</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+    </div>
+  )
 }
